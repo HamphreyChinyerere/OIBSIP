439b5fcc51dc9adfd1547f1e03329a767607608d
HamphreyChinyerere <hamphreytanatswa@gmail.com>
2026-08-26 14:12:41 +0200

    Added DOM elements

--- WebDev-L2-Calculator/script.js
+++ WebDev-L2-Calculator/script.js
@@ -2,3 +2,19 @@ let currentValue = "0";
 let previousValue = "";
 let operation = null;
 let shouldResetDisplay = false;
+
+const currentValueElement = document.getElementById("currentValue");
+const previousValueElement = document.getElementById("previousValue");
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+
+const clearButton = document.querySelector('[data-action="clear"]');
+const decimalButton = document.querySelector('[data-action="decimal"]');
+const backspaceButton = document.querySelector('[data-action="backspace"]');
+const equalsButton = document.querySelector('[data-action="equals"]');
+const signButton = document.querySelector('[data-action="toggle-sign"]');
+const percentageButton = document.querySelector('[data-action="percentage"]');
+
+const themeToggle = document.getElementById("themeToggle");
+
